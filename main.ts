322845895734bd769c03835dfde3622c6c617db1
JuanCFalla <j.fallag@uniandes.edu.co>
2022-04-14 17:01:58 -0500

    taller 1 completo

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import { Serie } from "./serie.js"
 
 let seriesTable: HTMLElement = document.getElementById("series")!;
 mostrarDatosSeries(series);
-
+mostrarPromedioTemporadas(series);
 
 function mostrarDatosSeries(series1: Serie[]){
     let seriesTBody: HTMLElement = document.createElement("tBody");
@@ -15,5 +15,21 @@ function mostrarDatosSeries(series1: Serie[]){
         <td>${serie.temporadas}</td>`;
         seriesTBody.appendChild(trElement);
     }
+    seriesTable.appendChild(seriesTBody);
+}
+
+function mostrarPromedioTemporadas(series: Serie[]){
+    let seriesTBody: HTMLElement = document.createElement("tBody");
+    let total_temporadas: number = 0;
+    let tamano: number = 0;
+    for (let serie of series){
+        let temporadas: number = serie.temporadas;
+        total_temporadas = total_temporadas + temporadas; 
+        tamano= tamano+1;
+    }
+    let promedio: number = total_temporadas/tamano;
+    let trElement: HTMLElement = document.createElement("tr");
+    trElement.innerHTML = `<td>Promedio de temporadas: ${promedio}</td>`;
+    seriesTBody.appendChild(trElement);    
     seriesTable.appendChild(seriesTBody);
 }
